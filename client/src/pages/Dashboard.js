0772a47c57a3587d1455018daa4876aa04442d02
ethan <ethanm113@gmail.com>
2022-12-06 18:11:21 -0600

    finished tutorial and working

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,10 +3,14 @@ import React, { useEffect } from 'react'
 // import jwt from 'jsonwebtoken'
 import jwt_decode from "jwt-decode";
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+
 
 const Dashboard = () => {
 
     const navigate = useNavigate()
+    const [quote, setQuote] = useState('')
+    const [tempQuote, setTempQuote] = useState('')
 
     async function populateQuote() {
         const req = await fetch('http://localhost:1337/api/quote', {
@@ -14,8 +18,12 @@ const Dashboard = () => {
                 'x-access-token': localStorage.getItem('token'),
             },
         })
-        const data = req.json()
-        console.log(data)
+        const data = await req.json()
+        if (data.status === 'ok') {
+            setQuote(data.quote)
+        } else {
+            alert(data.error)
+        }
     }
 
     useEffect(()=> {
@@ -33,7 +41,41 @@ const Dashboard = () => {
 
     }, [])
 
-    return <h1>Hello World</h1>
+    async function updateQuote(event) {
+        event.preventDefault()
+        const req = await fetch('http://localhost:1337/api/quote', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': localStorage.getItem('token'),
+            },
+            body: JSON.stringify({
+                quote: tempQuote,
+            }),
+        })
+        const data = await req.json()
+        if (data.status === 'ok') {
+            setQuote(tempQuote)
+            setTempQuote('')
+        } else {
+            alert(data.error)
+        }
+    }
+
+    return (
+        <div>
+            <h1>Your quote: {quote || 'No quote found'}</h1>
+            <form onSubmit={updateQuote}>
+                <input 
+                    type='text' 
+                    placeholder='Quote' 
+                    value={tempQuote} 
+                    onChange={e => setTempQuote(e.target.value) }
+                />
+                <input type="submit" value="Update Quote" />
+            </form>
+        </div>
+    )
 }
 
 export default Dashboard
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const User = require('./models/user.model')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 app.use(cors())
 app.use(express.json())
@@ -15,9 +16,10 @@ mongoose.connect('mongodb://localhost:27017/travelcounter')
 app.post('/api/register', async (req, res) => {
     console.log(req.body)
     try {
+        const newPassword = await bcrypt.hash(req.body.password, 10)
         await User.create({
             username: req.body.username,
-            password: req.body.password,
+            password: newPassword,
         })
         res.json({ status: 'ok'})
     } catch(err) {
@@ -30,10 +32,17 @@ app.post('/api/login', async (req, res) => {
 
     const user = await User.findOne({
         username: req.body.username,
-        password: req.body.password
+        // password: req.body.password
     })
 
-    if (user) {
+    if (!user) { return { status: 'error', error: 'Invalid Login' } }
+
+    const isPasswordValid = await bcrypt.compare(
+        req.body.password, 
+        user.password
+    )
+
+    if (isPasswordValid) {
         const token = jwt.sign({
             username: req.body.username,
             password: req.body.password,
@@ -42,6 +51,41 @@ app.post('/api/login', async (req, res) => {
         return res.json({ status: 'ok', user: token})
     } else {
         return res.json({ status: 'error', user: false})
+    }
+})
+
+app.get('/api/quote', async (req, res) => {
+
+    const token = req.headers['x-access-token']
+
+    try {
+        const decoded = jwt.verify(token, 'secret123')
+        const username = decoded.username
+        const user = await User.findOne({ username: username })
+
+        return res.json({ status: 'ok', quote: user.quote})
+    } catch(error) {
+        console.log(error)
+        res.json({ status: 'error', error: 'invalid token'})
+    }
+})
+
+app.post('/api/quote', async (req, res) => {
+
+    const token = req.headers['x-access-token']
+
+    try {
+        const decoded = jwt.verify(token, 'secret123')
+        const username = decoded.username
+        await User.updateOne(
+            { username: username }, 
+            { $set: { quote: req.body.quote } }
+        )
+
+        return res.json({ status: 'ok'})
+    } catch(error) {
+        console.log(error)
+        res.json({ status: 'error', error: 'invalid token'})
     }
 })
 
